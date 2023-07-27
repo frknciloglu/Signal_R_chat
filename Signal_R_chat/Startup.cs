9b3302b298658777fa19603e09c5e5ed646d1f78
frknciloglu <59924846+frknciloglu@users.noreply.github.com>
2023-07-27 13:36:01 +0300

    Add project files.

---
+++ Signal_R_chat/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(Signal_R_chat.Startup))]
+
+namespace Signal_R_chat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
